2778ae88b626b11ab446bec5d81dc96c9b497124
lorenzowind <lorenzomart01@gmail.com>
2023-06-14 22:06:22 -0300

    Reverted commits

--- studentService/src/main/java/com/studentService/studentService/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.studentService.studentService.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.studentService.studentService.model.Student;
-
-public interface StudentRepository extends MongoRepository<Student, String> {
-}
